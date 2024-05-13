bfb0669cfda8e0aec42a75b9b68b866aed71a353
Hagg-o7 <f20220326@pilani.bits-pilani.ac.in>
2024-05-13 15:57:37 +0530

    updated

--- uavsduyavsf.py
+++
@@ -1,9 +0,0 @@
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
-import numpy as np
-from qiskit_aer import AerSimulator
-from qiskit_aer.noise import NoiseModel
-import random
-import Alice, Bob
-
-AerSimulator.available_devices()
-
